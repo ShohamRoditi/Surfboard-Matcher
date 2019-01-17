4279df9230fee4a73fb938ee59557bd1b9c6e7fe
GoldenDude <edanazran@gmail.com>
2019-01-17 10:02:50 +0200

    Update server.js

--- service/server.js
+++ service/server.js
@@ -3,9 +3,9 @@ const   express      = require('express'),
         surfboardCtl = require('./controllers/surfboard.ctl'),
         port         = process.env.PORT || 3000;
 
-app.set('port',port);
+app.set('port', port);
 app.use('/', express.static('./public'));
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.set("Content-Type", "application/json");
@@ -15,8 +15,9 @@ app.use((req,res,next) => {
 /*** All routes ***/
 app.get('/getAllSurfboards', surfboardCtl.getAll);
 app.get('/matchSurfboard', surfboardCtl.getMatched);
-//app.get('/final-ideas/updateIdea', ideaCtl.updateData);
-//app.get('/final-ideas/deleteIdea', ideaCtl.deleteData);
+//app.put('/admin/getAllSurfboards', surfboardCtl.updateSurfboard);
+//app.post('/admin/matchSurfboard', surfboardCtl.addSurfboard);
+//app.delete('/admin/matchSurfboard', surfboardCtl.deleteSurfboard);
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);

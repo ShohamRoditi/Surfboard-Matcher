214f0a65a97fea9602d0e0719bd13a1e558c500a
GoldenDude <edanazran@gmail.com>
2019-01-17 08:13:21 +0200

    Update surfboard.ctl.js

--- service/controllers/surfboard.ctl.js
+++ service/controllers/surfboard.ctl.js
@@ -21,17 +21,17 @@ async function getWeather(){
 module.exports = {
 
     getAll: async function(req, res){
-        //const result = await Surfboard.find({});
-        const result = await getWeather();
-        console.log(result);
+        const result = await Surfboard.find({});
+        //const result = await getWeather();
+
         if(result)
             res.send(JSON.stringify(result));
         else res.status(404).send(`{"Failure": "No Documents Were Found"}`);
     },
 
     getMatched: async function(req, res){
-        const conditions = {height:{$gt: parseFloat(req.query.height) - 0.2, $lt: parseFloat(req.query.height) + 0.2}, userWeightMin: {$gt: parseFloat(req.query.weight) - 1},
-                            userWeightMax: {$lt: parseFloat(req.query.weight) + 11}};
+        const conditions = {height:{$gt: parseFloat(req.query.height), $lt: parseFloat(req.query.height) + 0.8}, userWeightMin: {$gt: parseFloat(req.query.weight) - 1},
+                            userWeightMax: {$lt: parseFloat(req.query.weight) + 20}};
         let result;
         console.log(conditions);
         result = await Surfboard.find(conditions);

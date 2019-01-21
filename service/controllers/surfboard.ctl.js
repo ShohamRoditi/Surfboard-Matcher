281f4ec60cb461879a2baf97e7d74466a6fdcbcb
ShohamRoditi <shohamroditi@gmail.com>
2019-01-21 12:38:08 +0200

    Revert "Update surfboard.ctl.js"
    This reverts commit d3e2d31b29de7b67f975750f0c55b3e7b1a1ada5.

--- service/controllers/surfboard.ctl.js
+++ service/controllers/surfboard.ctl.js
@@ -83,12 +83,14 @@ module.exports = {
         let minWeight = getRange(parseFloat(req.query.weight), parseInt(req.query.level));
         let swellSize =  await getWeather(req.query.location);
         const conditions = {height: {'$gt': parseFloat(req.query.height)}, userMinWeight: minWeight, maxSwell: {'$gt': swellSize}};
-
-        let result = await Surfboard.find(conditions);
-
-        if(result)
-            res.send(JSON.stringify(result));
-        else res.status(404).send(`{result: No Documents Were Found.}`);
+       
+        Surfboard.find(conditions).then(result => {
+            if(result)
+                res.send(JSON.stringify(result));
+            else res.status(404).send(`{result: No Documents Were Found.}`);
+        }, err =>{
+            res.status(404).send(`{result: No Documents Were Found.}`);
+        });
     },
 
     updateSurfboard: async function(req,res){
